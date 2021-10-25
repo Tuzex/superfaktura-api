0119738a9c38287451c4281589e1f5b57221f2f5
Edo Barnáš <iam@edobarnas.com>
2021-10-25 21:26:31 +0200

    Define new invoices list options

--- src/Api/Invoices/GetListOfInvoices.php
+++ src/Api/Invoices/GetListOfInvoices.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tuzex\Superfaktura\Api\Invoices;
 
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Tuzex\Superfaktura\Api\Endpoint;
 use Tuzex\Superfaktura\Api\OptionsResolverBuilder;
@@ -29,7 +30,41 @@ final class GetListOfInvoices implements Endpoint
                 function (OptionsResolver $optionsResolver): void {
                     $optionsResolver->define('type')
                         ->allowedTypes('string')
-                        ->info('Type of document (proforma, regular, ...). Use | as separator for multiple values.');
+                        ->default('regular')
+                        ->info('Type of document (proforma, regular, ...). Use "|" as separator for multiple values.');
+
+                    $optionsResolver->define('amount_from')
+                        ->allowedTypes('float')
+                        ->allowedValues(fn (float $value): bool => $value >= 0.0)
+                        ->info('Amount from');
+
+                    $optionsResolver->define('amount_to')
+                        ->allowedTypes('float')
+                        ->allowedValues(
+                            fn (float $value): bool => $value >= 0.0 && $value > $optionsResolver->offsetGet('amount_from')
+                        )
+                        ->info('Amount to');
+
+                    $optionsResolver->define('client_id')
+                        ->allowedTypes('int')
+                        ->info('Client ID');
+
+                    $optionsResolver->define('ignore')
+                        ->allowedTypes('string', 'int')
+                        ->info('IDs of invoices to be ignored. Use "|" as separator for multiple values.');
+
+                    $optionsResolver->define('order_no')
+                        ->allowedTypes('string')
+                        ->info('Order number, from which invoice is created');
+
+                    $optionsResolver->define('search')
+                        ->allowedTypes('string')
+                        ->normalize(fn (Options $options, string $value): string => base64_encode($value))
+                        ->info('Search string');
+
+                    $optionsResolver->define('tag')
+                        ->allowedTypes('int')
+                        ->info('Tag ID');
                 }
             );
 
