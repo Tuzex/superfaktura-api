8bb5efe278911ff671e9be6d4f507a7e4199ca3e
Edo Barnáš <iam@edobarnas.com>
2021-10-25 20:06:40 +0200

    Define API objects as a service

--- src/NativeApiClient.php
+++ src/NativeApiClient.php
@@ -6,22 +6,22 @@ namespace Tuzex\Superfaktura;
 
 use Tuzex\Superfaktura\Api\Invoices;
 use Tuzex\Superfaktura\Api\Stock;
-use Tuzex\Superfaktura\Http\HttpClient;
 
 final class NativeApiClient implements ApiClient
 {
     public function __construct(
-        private HttpClient $httpClient,
+        private Invoices $invoices,
+        private Stock $stock,
     ) {
     }
 
     public function invoices(): Invoices
     {
-        return new Invoices($this->httpClient);
+        return $this->invoices;
     }
 
     public function stock(): Stock
     {
-        return new Stock($this->httpClient);
+        return $this->stock;
     }
 }
