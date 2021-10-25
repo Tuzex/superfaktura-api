3d077ca8d3509478e58810f70d6a6e3893823ef7
Edo Barnáš <iam@edobarnas.com>
2021-10-25 13:36:28 +0200

    Add Invoices to the ApiClient

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Tuzex\Superfaktura;
 
+use Tuzex\Superfaktura\Api\Invoices;
 use Tuzex\Superfaktura\Api\Stock;
 
 interface ApiClient
 {
+    public function invoices(): Invoices;
+
     public function stock(): Stock;
 }
--- src/NativeApiClient.php
+++ src/NativeApiClient.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tuzex\Superfaktura;
 
+use Tuzex\Superfaktura\Api\Invoices;
 use Tuzex\Superfaktura\Api\Stock;
 use Tuzex\Superfaktura\Http\HttpClient;
 
@@ -12,6 +13,11 @@ final class NativeApiClient implements ApiClient
     public function __construct(
         private HttpClient $httpClient,
     ) {
+    }
+
+    public function invoices(): Invoices
+    {
+        return new Invoices($this->httpClient);
     }
 
     public function stock(): Stock
