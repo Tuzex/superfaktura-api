cc98af0de0709668a6c8be3bfa7e8130730bf394
Edo Barnáš <iam@edobarnas.com>
2021-10-25 13:50:21 +0200

    Optimize an internal configuration of OptionsResolverBuilder

--- src/Api/OptionsResolverBuilder.php
+++ src/Api/OptionsResolverBuilder.php
@@ -24,52 +24,58 @@ final class OptionsResolverBuilder
 
     public function addPagination(int $page = 1, int $perPage = 50): self
     {
-        $this->configurators[] = function (OptionsResolver $optionsResolver) use ($page, $perPage): void {
-            $optionsResolver->define('page')
-                ->required()
-                ->allowedTypes('int')
-                ->normalize(fn (Options $options, int $value): int => $value > 0 ? $value : 1)
-                ->default($page)
-                ->info('Page number');
+        $this->setCustom(
+            function (OptionsResolver $optionsResolver) use ($page, $perPage): void {
+                $optionsResolver->define('page')
+                    ->required()
+                    ->allowedTypes('int')
+                    ->normalize(fn (Options $options, int $value): int => $value > 0 ? $value : 1)
+                    ->default($page)
+                    ->info('Page number');
 
-            $optionsResolver->define('per_page')
-                ->required()
-                ->allowedTypes('int')
-                ->normalize(fn (Options $options, int $value): int => $value > 0 ? $value : 50)
-                ->default($perPage)
-                ->info('Number of results per page');
-        };
+                $optionsResolver->define('per_page')
+                    ->required()
+                    ->allowedTypes('int')
+                    ->normalize(fn (Options $options, int $value): int => $value > 0 ? $value : 50)
+                    ->default($perPage)
+                    ->info('Number of results per page');
+            }
+        );
 
         return $this;
     }
 
     public function addSorting(string $direction = 'DESC', string $attribute = 'regular_count'): self
     {
-        $this->configurators[] = function (OptionsResolver $optionsResolver) use ($direction, $attribute): void {
-            $optionsResolver->define('direction')
-                ->required()
-                ->allowedTypes('string')
-                ->allowedValues('ASC', 'DESC')
-                ->default($direction)
-                ->info('Sorting direction');
+        $this->setCustom(
+            function (OptionsResolver $optionsResolver) use ($direction, $attribute): void {
+                $optionsResolver->define('direction')
+                    ->required()
+                    ->allowedTypes('string')
+                    ->allowedValues('ASC', 'DESC')
+                    ->default($direction)
+                    ->info('Sorting direction');
 
-            $optionsResolver->define('sort')
-                ->allowedTypes('string')
-                ->default($attribute)
-                ->info('Sorting attribute');
-        };
+                $optionsResolver->define('sort')
+                    ->allowedTypes('string')
+                    ->default($attribute)
+                    ->info('Sorting attribute');
+            }
+        );
 
         return $this;
     }
 
     public function addListInfo(): self
     {
-        $this->configurators[] = function (OptionsResolver $optionsResolver): void {
-            $optionsResolver->define('listinfo')
-                ->allowedTypes('int')
-                ->allowedValues(0, 1)
-                ->info('Show meta information for results (1=yes)');
-        };
+        $this->setCustom(
+            function (OptionsResolver $optionsResolver): void {
+                $optionsResolver->define('listinfo')
+                    ->allowedTypes('int')
+                    ->allowedValues(0, 1)
+                    ->info('Show meta information for results (1=yes)');
+            }
+        );
 
         return $this;
     }
