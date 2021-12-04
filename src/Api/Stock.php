65094ce1eb0ad4e9a6208a946ea103005dff6ed5
Edo Barnáš <iam@edobarnas.com>
2021-12-04 09:21:34 +0100

    Use a specific `EndpointProvider` in the `Stock`

--- src/Api/Stock.php
+++ src/Api/Stock.php
@@ -7,24 +7,30 @@ namespace Tuzex\Superfaktura\Api;
 use Tuzex\Superfaktura\Api\Stock\GetListOfStockItems;
 use Tuzex\Superfaktura\Api\Stock\ViewStockItemDetails;
 use Tuzex\Superfaktura\Http\HttpClient;
+use Tuzex\Superfaktura\Service\Endpoint\EndpointDefinition;
+use Tuzex\Superfaktura\Service\Endpoint\EndpointProvider;
+use Tuzex\Superfaktura\Service\Endpoint\Provider\PersistentEndpointProvider;
 
 final class Stock
 {
+    private EndpointProvider $endpointProvider;
+
     public function __construct(
         private HttpClient $httpClient
     ) {
+        $this->endpointProvider = PersistentEndpointProvider::setup(
+            new EndpointDefinition(GetListOfStockItems::class, fn () => new GetListOfStockItems($this->httpClient))
+        );
     }
 
     public function listItems(array $options = []): Collection
     {
-        $endpoint = new GetListOfStockItems($this->httpClient);
-
-        return new Collection($endpoint, $options);
+        return new Collection($this->endpointProvider->provide(GetListOfStockItems::class), $options);
     }
 
     public function searchItems(string $keyword, array $options = []): Collection
     {
-        $endpoint = new GetListOfStockItems($this->httpClient);
+        $endpoint = $this->endpointProvider->provide(GetListOfStockItems::class);
         $options = array_merge($options, [
             'search' => $keyword,
         ]);
@@ -34,7 +40,7 @@ final class Stock
 
     public function searchItemsBySku(string $sku, array $options = []): Collection
     {
-        $endpoint = new GetListOfStockItems($this->httpClient);
+        $endpoint = $this->endpointProvider->provide(GetListOfStockItems::class);
         $options = array_merge($options, [
             'sku' => $sku,
         ]);
